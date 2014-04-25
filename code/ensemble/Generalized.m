e2f37c33850d8396dc9bc715106cb642fa4b3d6c
Xiao-Feng Xie <xfxie@cs.cmu.edu>
2014-04-25 15:34:01 -0400

    None

--- code/ensemble/Generalized.m
+++ code/ensemble/Generalized.m
@@ -8,12 +8,13 @@ function GD=Generalized(C)
 % C (MxN) M represents number of objects, N represents number of
 % classifiers, T is the labels of the objects (0 represents  false, 1 true)
 
+m = size(C,1);
+L = size(C,2);
+Nt=zeros(m,1);
 
-Nt=zeros(size(C,1),1);
 
-
-for i=1:size(C,1)
-   for j=1:size(C,2)
+for i=1:m
+   for j=1:L
      if(C(i,j)==1)
          Nt(i,1)=Nt(i,1)+1;
      end
@@ -21,8 +22,8 @@ for i=1:size(C,1)
 end
 
 
-P=zeros(3,(size(C,2)+1));
-for i=1: size(C,1)
+P=zeros(3,(L+1));
+for i=1: m
    P(1,1+Nt(i,1))=P(1,1+Nt(i,1)) +1;
     
 end
@@ -30,19 +31,19 @@ end
 P=P/10;
 
 for i=1:size(P,2)
-   P(2,i)=(i-1)/size(C,2); 
+   P(2,i)=(i-1)/L; 
 end
 
 
 for i=2:size(P,2)
-   P(3,i)=(i-2)/(size(C,2)-1) ;
+   P(3,i)=(i-2)/(L-1) ;
 end
 
 
 
-Y=zeros(1,size(C,2)+1);
-Y2=zeros(1,size(C,2)+1);
-Pr=zeros(1,size(C,2)+1);
+Y=zeros(1,L+1);
+Y2=zeros(1,L+1);
+Pr=zeros(1,L+1);
 
 for i=1:size(P,2)
    Y(1,i)=1-P(2,(size(P,2)+1)-i);
--- code/ensemble/runEnsemble.m
+++ code/ensemble/runEnsemble.m
@@ -3,15 +3,15 @@
 
 clear;
 
-DevNames = ['Basic';'Disagreement'; 'Correlation'; 'Q_Test'; 'Double_Fault'; 'Coincident_Failure'; 'entropy'; 'Interrater_k'; 'Kohavi_Wolpert'];
-TYPEs=[-1; 3; 2; 2; 2; 4; 1; 1; 0];
+DevNames = ['Basic';'Disagreement'; 'Correlation'; 'Q_Test'; 'Double_Fault'; 'Coincident_Failure'; 'entropy'; 'Interrater_k'; 'Kohavi_Wolpert'; 'Generalized'];
+TYPEs=[-1; 3; 2; 2; 2; 4; 1; 1; 0; 1];
 WeiNames = ['Basic';'PerformanceWeighting'];
 
 k=7;
 TSIZE=size(DevNames, 1);
 WSIZE=size(WeiNames, 1);
 n =0;
-for k=3:8
+for k=3:7
 filename=sprintf('result/errors_%d.csv', k);
 fid = fopen(filename, 'w');
 for j=1:WSIZE
